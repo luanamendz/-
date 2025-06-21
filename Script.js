c99166f03413bbffe4b10c3070e27d3e31d8bf5f
luanamendz <luana.mendes.ferr@gmail.com>
2025-06-21 20:02:08 -0300

    script.js

--- Script.js
+++
@@ -1,7 +0,0 @@
-const botao = document.getElementById('botao');
-const player = document.getElementById('player');
-
-botao.addEventListener('click', () => {
-  botao.style.display = 'none';
-  player.classList.remove('oculto');
-});
---
+++ script.js
@@ -0,0 +1,22 @@
+const block = document.querySelector('.block');
+
+const heartPositions = [
+  [0, 4],[1,2],[1,6],[2,1],[2,3],[2,5],[2,7],
+  [3,0],[3,2],[3,4],[3,6],[3,8],
+  [4,1],[4,3],[4,5],[4,7],
+  [5,2],[5,6],
+  [6,4]
+];
+
+heartPositions.forEach((pos, index) => {
+  const heart = document.createElement('heart');
+  heart.style.left = `${pos[1] * 30}px`;
+  heart.style.top = `${pos[0] * 30}px`;
+  heart.style.opacity = 0;
+  block.appendChild(heart);
+
+  setTimeout(() => {
+    heart.style.transition = 'opacity 0.5s ease';
+    heart.style.opacity = 1;
+  }, index * 150);
+});
